a590b8254ee1d0810f32e09af9b0a0447a428afc
Matthew Nweke <nwekematthew243@gmail.com>
2024-07-03 13:59:15 +0100

    image

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,49 +1,63 @@
-import { useContext, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import '../../css/index.css'
 
 const Header = () => {
   const [isMenuOpen, setMenuOpen] = useState(false);
   const [isScrolled, setScrolled] = useState(false);
 
-  const toggleMenu = () => {
+  const toggleMenu = (event) => {
+    event.stopPropagation();
     setMenuOpen(!isMenuOpen);
   };
+
   const handleScroll = () => {
     const scrollThreshold = 50;
     setScrolled(window.scrollY > scrollThreshold);
   };
 
+  const handleClickOutside = (event) => {
+    if (
+      !event.target.closest(".menu") &&
+      !event.target.closest(".hamburger-button")
+    ) {
+      setMenuOpen(false);
+    }
+  };
+
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
+    document.addEventListener("click", handleClickOutside);
 
     return () => {
       window.removeEventListener("scroll", handleScroll);
+      document.removeEventListener("click", handleClickOutside);
     };
   }, []);
 
   return (
     <div
-      className={`w-full  flex shadow-md fixed z-40 top-0  left-0 justify-around max-lg:block transition-all duration-300 ${
+      className={`w-full flex shadow-md fixed z-40 top-0 left-0 justify-around max-lg:block transition-all duration-300 ${
         isScrolled ? "bg-[transparent] bg-opacity-90" : "bg-[#ff0000]"
       }`}
     >
-      <div className="flex justify-between items-center py-4  px-5 lg:px-10 xl:px-20">
+      <div className="flex justify-between items-center py-2 px-5 lg:px-10 xl:px-20">
         <Link to="/">
-          {" "}
           <img
-            src="/FxLogo.png"
-            className="cursor-pointer"
-            width={200}
-            height={50}
-            alt=""
-          />{" "}
+            src="/logo.svg"
+            className="cursor-pointer h-[4.5rem] w-[4.5rem]"
+            
+            alt="Logo"
+          />
         </Link>
 
         {/* Responsive Hamburger Menu */}
         <div className="lg:hidden">
           <button
             onClick={toggleMenu}
-            className="text-white focus:outline-none bg-[#ffcc29] p-2"
+            className={`hamburger-button text-white focus:outline-none bg-[#ffcc29] p-2 transition-transform duration-300 ${
+              isMenuOpen ? "rotate-90" : "rotate-0"
+            }`}
           >
             <svg
               className="h-7 w-7"
@@ -65,53 +79,44 @@ const Header = () => {
 
       {/* Dropdown Menu */}
       <div
-        className={`lg:hidden w-[50%] left-1/2 -translate-x-1/2 absolute transition-all duration-300 overflow-hidden max-md:w-[60%] max-sm:w-[70%] ${
+        className={`menu lg:hidden w-[50%] left-1/2 -translate-x-1/2 absolute transition-all duration-300 overflow-hidden max-md:w-[60%] max-sm:w-[70%] ${
           isMenuOpen ? "max-h-[60vh] py-10" : "max-h-0"
         } ${isScrolled ? "bg-[#ffcc29] bg-opacity-60" : "bg-[#ffcc29]"}`}
       >
-        <div className="flex flex-col px-3 gap-4 ">
+        <div className="flex flex-col px-3 gap-4">
           <Link to="/">
-            <p className=" p-3 hover:bg-[#dddddd] rounded-lg text-white text-sm cursor-pointer max-lg:text-[1rem]">
+            <p className="p-3 font-semibold rounded-lg text-white text-sm cursor-pointer max-lg:text-[1rem]">
               Home
             </p>
           </Link>
           <div className="cursor-pointer">
             <Link to="/plan">
-              <p className="p-3 rounded-lg text-white text-sm max-lg:text-[1rem] hover:bg-[#dddddd]">
+              <p className="p-3 rounded-lg text-white text-sm max-lg:text-[1rem] font-semibold">
                 Menu
               </p>
             </Link>
           </div>
           <div className="cursor-pointer">
             <Link to="/about">
-              <p className="p-3 rounded-lg text-white text-sm max-lg:text-[1rem] hover:bg-[#dddddd]">
+              <p className="p-3  rounded-lg text-white text-sm max-lg:text-[1rem] font-semibold">
                 About Us
               </p>
             </Link>
           </div>
           <div className="cursor-pointer">
             <Link to="/contact">
-              <p className="p-3 rounded-lg text-white text-sm max-lg:text-[1rem] hover:bg-[#dddddd]">
+              <p className="p-3 rounded-lg text-white text-sm max-lg:text-[1rem] font-semibold">
                 Services
               </p>
             </Link>
           </div>
           <div className="cursor-pointer">
             <Link to="/contact">
-              <p className="p-3 rounded-lg text-white text-sm max-lg:text-[1rem] hover:bg-[#dddddd]">
+              <p className="p-3 rounded-lg text-white text-sm max-lg:text-[1rem] font-semibold">
                 Contact
               </p>
             </Link>
           </div>
-          <div className="cursor-pointer">
-            <Link to="/signin">
-              <p className=" text-white bg-[#ff0000] inline-block p-3 rounded-lg text-sm max-lg:text-[1rem]">
-                Sign in
-              </p>
-            </Link>
-          </div>
-
-         
         </div>
       </div>
 
@@ -119,7 +124,7 @@ const Header = () => {
       <div className={`lg:flex items-center gap-16 hidden`}>
         <div>
           <Link to="/">
-            <p className="font-semibold text-white text-sm cursor-pointer">
+            <p className="font-semibold  text-white  cursor-pointer">
               Home
             </p>
           </Link>
@@ -127,32 +132,21 @@ const Header = () => {
 
         <div className="cursor-pointer">
           <Link to="/plan">
-            <p className="font-semibold text-white text-sm">Plan</p>
+            <p className="font-semibold text-white ">Plan</p>
           </Link>
         </div>
         <div className="cursor-pointer">
           <Link to="/about">
-            <p className="font-semibold text-white text-sm">About</p>
+            <p className="font-semibold text-white ">About</p>
           </Link>
         </div>
         <div className="cursor-pointer">
           <Link to="/contact">
-            <p className="font-semibold text-white text-sm">Contact</p>
+            <p className="font-semibold text-white ">Contact</p>
           </Link>
         </div>
-        <div className="cursor-pointer">
-          <Link to="/signin">
-            <p className="font-semibold text-white text-sm max-lg:text-[1.2rem]">
-              Sign in
-            </p>
-          </Link>
-        </div>
-        <div className="flex items-center gap-2 cursor-pointer"></div>
-        <div className="cursor-pointer">
-          <select name="language" id="language" className="cursor-pointer">
-            <option value="language">English</option>
-          </select>
-        </div>
+
+        
       </div>
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,9 @@ function Home() {
   return (
     <div>
       <Layout>
-        <h1>Home Page</h1>
+      <div className="relative bg-cover bg-center h-[50vh] md:h-[60vh] lg:h-[70vh] mt-20" style={{ backgroundImage: 'url(/img3.jpg)' }}>
+         
+        </div>
       </Layout>
     </div>
   );
